a0e1cf57d88885d9474e043cbebebd77b260cb5e
garinvit <garin.vitaly@yandex.ru>
2020-02-07 21:26:55 +0700

    Fix errors in Postgres HW

--- netology_db/main.py
+++ netology_db/main.py
@@ -5,60 +5,72 @@ connect_db = psycopg2.connect("dbname=netology_db user=netology")
 cur = connect_db.cursor()
 
 students = [
-    {'id': 1, 'name': 'Иванов Иван', 'gpa': 4, 'birth': '1998-02-03'},
-    {'id': 2, 'name': 'Петров Петр', 'gpa': 4.5, 'birth': '1999-03-04'},
-    {'id': 3, 'name': 'Иванов Петр', 'gpa': 4.2, 'birth': '2000-04-05'},
+    {'name': 'Иванов Иван', 'gpa': 4, 'birth': '1998-02-03'},
+    {'name': 'Петров Петр', 'gpa': 4.5, 'birth': '1999-03-04'},
+    {'name': 'Иванов Петр', 'gpa': 4.2, 'birth': '2000-04-05'},
 ]
-student =  {'id': 4, 'name': 'Другой Студент', 'gpa': 3.9, 'birth': '1999-11-11'}
+
+courses = [
+    {'name': 'Программирование'},
+    {'name': 'Математика'},
+    {'name': 'Операционные системы и среды'},
+]
+student = {'name': 'Другой Студент', 'gpa': 3.9, 'birth': '1999-11-11'}
 
 def create_db(): # создает таблицы
     cur.execute("""
-    CREATE TABLE student (
+    CREATE TABLE if not exists student (
     id serial PRIMARY KEY not null, 
     name varchar(100) not null, 
     gpa numeric(10, 2), 
     birth timestamp with time zone);
     
-    CREATE TABLE course (
+    CREATE TABLE if not exists course (
     id serial primary key  not null, 
-    name varchar(100) not null,
-    primary key (id, name));
+    name varchar(100) not null);
     
-    CREATE TABLE student_course (
-    id serial primary key  not null, 
-    name varchar(100) not null,
-    primary key (id, name))
-    birth timestamp;
-    
-     
+    CREATE TABLE if not exists student_course (
+    id serial PRIMARY KEY,
+    student_id INTEGER REFERENCES student(id),
+    course_id INTEGER REFERENCES  course(id));
     """)
     connect_db.commit()
 
 
 def get_students(course_id): # возвращает студентов определенного курса
-    cur.execute("select name from course where course.id = (%s)", str(course_id))
-    course = cur.fetchall()
-    print(course)
-    return course
-
-
-def get_students_add_birth(course_id):
+    # cur.execute("select student.name from student_course where student_course.course_id = (%s)", str(course_id))
     cur.execute("""
-    select student.id, student.name, student.birth, course.id
-    from course
-    inner join student on course.name = student.name
-    where course.id = (%s)""", str(course_id))
+    select s.id, s.name, c.name 
+    from student_course sc
+    join student s on s.id = sc.student_id 
+    join course c on c.id = sc.course_id
+    where c.id = (%s)""", str(course_id))
     course = cur.fetchall()
     print(course)
     return course
+
+
+def add_courses(courses_id):
+    try:
+        for course in courses_id:
+            cur.execute("insert into course (name) values (%s)", (course['name'], ))
+
+    except Exception as e:
+        print(e)
+        connect_db.rollback()
+    else:
+        connect_db.commit()
+        print("Курсы добавлены ")
 
 
 def add_students(course_id, students): # создает студентов и
                                        # записывает их на курс
     try:
         for student in students:
-            cur.execute("insert into student (id, name, gpa, birth) values (%s, %s, %s, %s)", (student['id'], student['name'], student['gpa'], student['birth']))
-            cur.execute("insert into course (id, name) values (%s, %s)", (course_id, student['name']))
+            cur.execute("insert into student (name, gpa, birth) values (%s, %s, %s)", (student['name'], student['gpa'], student['birth']))
+            cur.execute("select student.id from student where student.name = (%s)", (student['name'],))
+            stud_id = cur.fetchall()[0][0]
+            cur.execute("insert into student_course (student_id, course_id ) values (%s, %s)", (stud_id, course_id, ))
     except Exception as e:
         print(e)
         connect_db.rollback()
@@ -68,7 +80,7 @@ def add_students(course_id, students): # создает студентов и
 
 
 def add_student(student): # просто создает студента
-    cur.execute("insert into student (id, name, gpa, birth) values (%s, %s, %s, %s)", (student['id'], student['name'], student['gpa'], student['birth']))
+    cur.execute("insert into student (name, gpa, birth) values (%s, %s, %s)", (student['name'], student['gpa'], student['birth']))
     connect_db.commit()
 
 
@@ -81,12 +93,11 @@ def get_student(student_id):
 
 def main():
     create_db()
+    add_courses(courses)
     add_students(1, students)
     add_student(student)
     get_student('2')
     get_students(1)
-    get_students_add_birth(1)
-
 
 if __name__ == "__main__":
     main()
